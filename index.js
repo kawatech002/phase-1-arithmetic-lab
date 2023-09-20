967feaa66c509ddc17cde0343180dc2afb1a03a8
kawatech002 <kawanjiku94@gmail.com>
2023-09-20 16:29:03 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 31;
+
+let num2 = 2; 
+
+let multiply = num1 * num2;
+
+let num3 = 22;
+
+let num4 = 9;
+
+let mod = num3 % num4;
+
+let numberArray = [10, 5, 20, 15];
+
+let max = Math.max(...numberArray);
+
+let random = Math.floor(Math.random() * 100) +1;
